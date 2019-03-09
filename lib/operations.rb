272a2d23c9bf465f8a051812e4941e234dcf5b09
Nash Brancic <nashfft@gmail.com>
2019-03-09 03:54:34 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,14 +1,13 @@
 def unsafe?(speed)
 	if speed > 60 || speed < 40
 		true
-	else
+	elsif
 		false
 	end
 end
 
 def not_safe?(speed)
-  speed > 60
-	
+  speed > 60 || speed < 40 ? true : false
 end
 	
 
