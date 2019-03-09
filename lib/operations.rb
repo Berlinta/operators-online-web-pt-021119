02eaec6b3d78e3827adb24611cbdf7a72c9c645c
Nash Brancic <nashfft@gmail.com>
2019-03-09 03:50:02 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -2,8 +2,9 @@ def unsafe?(answer)
   answer = 79
   if answer > 60
     true
-  elsif answer 
-  answer >= 60 ? true : false
+  elsif answer.between(40, 60)
+    false
+  # answer >= 60 ? true : false
 end
 
 
